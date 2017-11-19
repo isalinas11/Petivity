840e1a419c88c61a0be831bf2859d67588ced368
Ivan Salinas <ivan.salinas1991@gmail.com>
2017-11-19 15:19:33 -0800

    Added mobile app

---
+++ mobile-app/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+
+export default class App extends Component {
+    render() {
+        return (
+            <Text>Hello world!</Text>
+        );
+    }
+}
