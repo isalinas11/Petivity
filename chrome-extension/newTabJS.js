141e833cc61b30186789cd008b7fd71bd98ed5b5
Sidra <sidra725@gmail.com>
2017-11-19 14:56:15 -0800

    added js file

---
+++ chrome-extension/newTabJS.js
@@ -0,0 +1,25 @@
+'use strict';
+
+controllerName.controller('NewTabController', ['$scope', '$resource',
+   function ($scope, $resource) {
+       
+       var CrabObject = {imageFile: filename.exe};
+       
+       var petArray = [crabObject, dogObject, TurtleObject];
+       /*
+        <ng-image {{$scope.main.selectedPet}}>
+       */
+       /*
+
+       var UsersModel = $resource('/user/list');
+       var usersModel = UsersModel.query({}, function (){
+
+           $scope.main.selectedPet = selectedPet;
+           
+ 
+       
+       });
+       */
+
+   }]);
+
