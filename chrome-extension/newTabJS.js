a6c9a765114bd3feba138203a0e4275d1649df3e
Sidra <sidra725@gmail.com>
2017-11-19 16:16:20 -0800

    s

--- chrome-extension/newTabJS.js
+++ chrome-extension/newTabJS.js
@@ -15,12 +15,38 @@ controllerName.controller('NewTabController', ['$scope', '$resource',
        var usersModel = UsersModel.query({}, function (){
 
            $scope.main.selectedPet = selectedPet;
-           
- 
-        startBlocking(){
         
+        Function to start blocking of websites
+        $scope.startBlocking = function(){
         ///function to start blocking websites
         }
+        
+       Fuction to  add websites
+        $scope.AddWebsites = function(){
+        
+        }
+        Function to select a pet
+        $scope.selectPet = function(){
+        
+        }
+        
+        Function to go to the settings section
+        $scope.goToSettings = function(){
+        
+        }
+        
+        $scope.selectHardOrSoftBlock = function(){
+        
+        }
+        
+        $scope.addInterests = function(){
+        
+        }
+        
+        $scope.EnableDisableWebsites = funciton(){
+        
+        }
+        
        
        });
        */
